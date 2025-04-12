551fc7a129f62be362ba2dcfa42b3597a4d9b11d
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-04-11 21:12:43 -0400

    fixed title

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -17,7 +17,7 @@ async function NavBar() {
               <path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
           </div>
-          <span className="text-white text-xl font-bold tracking-tight">Next<span className="text-green-400">Play</span></span>
+          <span className="text-white text-xl font-bold tracking-tight">Next<span className="text-green-400">Playground</span></span>
         </Link>
 
         {isLoggedIn ? (
