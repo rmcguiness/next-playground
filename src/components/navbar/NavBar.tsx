c735b26faeb82a266e7f1746a8b8d4543c5262a8
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-04-13 00:03:12 -0400

    updated navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -22,7 +22,7 @@ async function NavBar() {
 
         {isLoggedIn ? (
           <div className="relative group">
-            <button className="flex items-center space-x-2 text-white bg-slate-700 hover:bg-slate-600 px-4 py-2 rounded-full transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-400">
+            <button className="flex items-center space-x-2 text-white bg-slate-700 hover:bg-slate-600 px-6 py-2 rounded-full transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-400">
               <span>Menu</span>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
