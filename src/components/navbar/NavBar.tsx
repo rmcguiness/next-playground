2285f559269ae5b21364d09f59f6ba7e1460a279
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-04-16 15:34:44 -0400

    updated nav and icons

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -53,9 +53,16 @@ async function NavBar() {
                   </a>
                   <a href="/protected/sticky-components" className="flex items-center px-4 py-3 text-gray-800 hover:bg-gray-50 transition duration-200">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25" />
                     </svg>
+
                     Sticky Components
+                  </a>
+                  <a href="/protected/carousel" className="flex items-center px-4 py-3 text-gray-800 hover:bg-gray-50 transition duration-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                    </svg>
+                    Carousel
                   </a>
                 </DropDown>
 
