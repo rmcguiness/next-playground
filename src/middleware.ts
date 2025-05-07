df9bc1f125ac2fdca918a4cb7a440a9c6ae8433f
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-05-07 13:58:53 -0400

    updated csp to allow resume site to show preview

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export async function middleware(request: NextRequest) {
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors 'none';
+    frame-ancestors 'none' https://rmcguiness.com;
     upgrade-insecure-requests;
   `
   // Replace newline characters and spaces
